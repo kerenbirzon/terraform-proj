b73db68cb36483a84096764289d02f3de103cff1
kerenbirzon <kerenbirzon4@gmail.com>
2023-11-04 14:26:04 +0200

    addons + helm deployments

---
+++ addons.tf
@@ -0,0 +1,37 @@
+################################################################################
+# EKS Addons
+################################################################################
+
+module "addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.1"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn  
+
+  # EKS Addons
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+    coredns    = {
+      prepare = true
+      most_recent = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      preserve    = true
+      most_recent = true 
+
+      timeouts = {
+        create = "25m"
+        delete = "10m"
+      }
+    }
+  }
+  
+  enable_cluster_autoscaler = true
+  enable_metrics_server = true
+}
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = local.region
 }
+
 data "aws_availability_zones" "available" {}
 
 locals {
@@ -11,6 +12,31 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
 }
 
 ################################################################################
@@ -31,23 +57,11 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   
   enable_irsa = true
-
+  
+  #   - It should have one cluster and one additional security group 
+  create_cluster_security_group = true
   cluster_additional_security_group_ids = [aws_security_group.additional_sg_1.id]
-
-  cluster_addons = {
-    coredns = {
-      preserve    = true
-      most_recent = true
-
-      timeouts = {
-        create = "25m"
-        delete = "10m"
-      }
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+  
   #   - It should have one node group with minumum 1 and maximum 5 ondemand nodes of any EC2 type  
   eks_managed_node_groups = {
     general = {
@@ -61,40 +75,24 @@ module "eks" {
 
 }
 
-################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.vpc_name
-
-  cidr = local.vpc_cidr
-  azs  = local.azs
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.3.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = false
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
-#   - It should have one cluster and one additional security group 
 resource "aws_security_group" "additional_sg_1" {
   name_prefix = "additional-sg-1-"
   vpc_id      =  module.vpc.vpc_id
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
+
